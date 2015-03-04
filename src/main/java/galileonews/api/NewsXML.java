e564ecd892846af24f30a0f449d012b851be1764
Samuel Franklyn <sfranklyn@gmail.com>
2015-03-04 15:33:00 +0700

    Implement Galileo News

---
+++ src/main/java/galileonews/api/NewsXML.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2015 Samuel Franklyn <sfranklyn@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package galileonews.api;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Samuel Franklyn <sfranklyn@gmail.com>
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class NewsXML {
+
+    private String userName;
+    private String password;
+    private String pcc;
+    private String gtid;
+    private String signon;
+    private Boolean important;
+    private Boolean ascending;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPcc() {
+        return pcc;
+    }
+
+    public void setPcc(String pcc) {
+        this.pcc = pcc;
+    }
+
+    public String getGtid() {
+        return gtid;
+    }
+
+    public void setGtid(String gtid) {
+        this.gtid = gtid;
+    }
+
+    public String getSignon() {
+        return signon;
+    }
+
+    public void setSignon(String signon) {
+        this.signon = signon;
+    }
+
+    public Boolean getImportant() {
+        return important;
+    }
+
+    public void setImportant(Boolean important) {
+        this.important = important;
+    }
+
+    public Boolean getAscending() {
+        return ascending;
+    }
+
+    public void setAscending(Boolean ascending) {
+        this.ascending = ascending;
+    }
+
+}
